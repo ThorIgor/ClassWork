062557579830db57c31acb7ad84d73cab5ecce19
user <user@A24COMP9>
2019-04-13 17:23:05 +0300

    New progect ( Events )

---
+++ IgorPr(2)/IgorPr(2)/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgorPr_2_
+{
+    delegate void GreenhouseDel(Greenhouse gh); 
+    class Greenhouse
+    {
+        double curT, maxT, minT;
+        public event GreenhouseDel TooHot, TooCold, Well;
+
+        public Greenhouse(double curt, double maxt, double mint)
+        {
+            curT = curt;
+            maxT = maxt;
+            minT = mint;
+        }
+        public double CurrentT
+        {
+            get { return curT; }
+            set
+            {
+                curT = value;
+                if (curT > maxT)
+                    TooHot(this);
+                else if (curT < minT)
+                    TooCold(this);
+                else
+                    Well(this);
+            }
+        }
+        public void print()
+        {
+            Console.WriteLine($"Temperature: {curT}, Max temperatur: {maxT}, Min temperature: {minT}");
+        }
+    }
+    class Heater
+    {
+        public static void Warm(Greenhouse gh)
+        {
+            Console.WriteLine($"Heater is working. Temperature: {gh.CurrentT}");
+            gh.CurrentT += 5;
+           
+        }
+    }
+    class Cooler
+    {
+        public static void Cool(Greenhouse gh)
+        {
+            Console.WriteLine($"Cooler is working. Temperature: {gh.CurrentT}");
+            gh.CurrentT -= 5;
+           
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Greenhouse house = new Greenhouse(20, 28, 24);
+            house.TooCold += Heater.Warm;
+            house.TooHot += Cooler.Cool;
+            house.Well += (Greenhouse gh) => Console.WriteLine($"Temperature is well. Temperature: {gh.CurrentT}");
+            while (true)
+            {
+                Console.Clear();
+                double TemChange = rand.Next(-5, 6);
+                house.print();
+                Console.WriteLine($"Temperature change: {TemChange}");
+                house.CurrentT += TemChange;
+                string end = Console.ReadLine();
+                if (end == "exit" || end == "Exit")
+                    break;
+            }
+        }
+    }
+}
---
+++ IgorPr/IgorPr/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgorPr_1_
+{
+    delegate void Del(int a, ConsoleColor c);
+    
+    class Program
+    {
+        static void DrawTriangle(int height, ConsoleColor c)
+        {
+            Console.ForegroundColor = c;
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void DrawSquere(int height, ConsoleColor c)
+        {
+            Console.ForegroundColor = c;
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= height; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void Main(string[] args)
+        {
+            Del d = DrawTriangle;
+            d += DrawSquere;
+            d(5, ConsoleColor.DarkYellow);
+
+
+        }
+    }
+}
