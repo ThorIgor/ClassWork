7fd4861584074b2a55b6720a614ebc1bd071f394
Ігор Тхорук <igoeee0@gmail.com>
2019-04-20 18:50:52 +0300

    New lesson

---
+++ IgorPr(3)/IgorPr(3)/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IgorPr_3_
+{
+    class Program
+    {
+        static void Swap<T>(ref T a, ref T b)
+        {
+            T pr = a;
+            a = b;
+            b = pr;
+        }
+        static void Sort<T>(ref T[] arr) where T : IComparable
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                    if (arr[i].CompareTo(arr[j]) < 0)
+                        Swap(ref arr[i], ref arr[j]);
+            }
+        }
+
+
+        class Product : IComparable
+
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+
+            public Product(string n = "Nothing", double p = 0)
+            {
+                Name = n;
+                Price = p;
+            }
+
+            public int CompareTo(object obj)
+            {
+                if (obj == null)
+                    throw new ArgumentNullException();
+                if (!(obj is Product))
+                    throw new ArgumentException();
+                return Price.CompareTo((obj as Product).Price);
+            }
+            public override string ToString()
+            {
+                return $"{Name}: {Price}";
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rand.Next(0, 41);
+
+            foreach (var el in arr)
+                Console.Write($"{el,3}");
+            Console.WriteLine();
+
+            Sort(ref arr);
+
+            foreach (var el in arr)
+                Console.Write($"{el,3}");
+            Console.WriteLine();
+
+            Product[] prod = new Product[] {
+                new Product("Fish", rand.Next(40, 501)),
+                new Product("Meet", rand.Next(30, 101)),
+                new Product("Bread", rand.Next(8, 20))
+            };
+
+            foreach (var el in prod)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+
+            Sort(ref prod);
+
+            foreach (var el in prod)
+                Console.Write($"{el} ");
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ IgorPr(4)/IgorPr(4)/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IgorPr_4_
+{
+    class Program
+    {
+        class Student
+        {
+            public string Name { get; set; }
+            public int[] Marks { get; set; }
+
+            public Student(string name, int[] marks)
+            {
+                Name = name;
+                Marks = marks;
+            }
+            public override string ToString()
+            {
+                string student = "";
+                student += Name + ": ";
+                foreach (var el in Marks)
+                    student += el + " ";
+                return student;
+
+            }
+            public byte[] ByteArray
+            {
+                get { return Encoding.Default.GetBytes(this.ToString()); }
+            }
+        }
+        static void CodeFile(string file, int num)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    byte[] arr = new byte[fs.Length];
+                    fs.Read(arr, 0, arr.Length);
+                    string str = Encoding.Default.GetString(arr);
+                    char[] chArr = str.ToCharArray();
+                    for (int i = 0; i < chArr.Length; i++)
+                        chArr[i] = (char)((int)chArr[i] + num);
+                    str = "";
+                    foreach (var el in chArr)
+                        str += el;
+                    fs.Seek(0, SeekOrigin.Begin);
+                    fs.Write(Encoding.Default.GetBytes(str), 0, Encoding.Default.GetBytes(str).Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void PrintFile(string file)
+        {
+            using (FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                byte[] result = new byte[fs.Length];
+                fs.Read(result, 0, result.Length);
+                Console.WriteLine(Encoding.Default.GetString(result));
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student student = new Student("Igor", new int[]{10, 12, 9, 12});
+            using (FileStream fs = new FileStream("File.txt", FileMode.Create))
+                fs.Write(student.ByteArray, 0, student.ByteArray.Length);
+            PrintFile("File.txt");
+            CodeFile("File.txt", 2);
+            PrintFile("File.txt");
+            CodeFile("File.txt", -2);
+            PrintFile("File.txt");
+        }
+    }
+}
