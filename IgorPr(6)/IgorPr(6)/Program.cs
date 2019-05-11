a8c5f54a1bb8f25608905a58f3396edfa04198cd
Ігор Тхорук <igoeee0@gmail.com>
2019-05-11 18:34:41 +0300

    IgorPr(6)(Task1)

---
+++ IgorPr(6)/IgorPr(6)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace IgorPr_6_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write somethin: ");
+            string input = Console.ReadLine();
+            string patern = @"(\b\d\w+ | \w+\d\b)";
+            Console.WriteLine("Result: " + Regex.Match(input, patern).Length);
+
+        }
+    }
+}
